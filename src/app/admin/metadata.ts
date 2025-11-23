f0c5d9660640f371b05dab0bd2f47f1bda28e27a
Hazrat Ummar Shaikh <hazratummar9@gmail.com>
2025-11-23 18:37:45 +0530

    feat: prevent search engine indexing and crawling of admin and API routes

---
+++ src/app/admin/metadata.ts
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    robots: {
+        index: false,
+        follow: false,
+        nocache: true,
+        googleBot: {
+            index: false,
+            follow: false,
+        },
+    },
+};
